178ac28a32a0a7166251b01cacc818e9683d5bc1
Шарифзода Икбол <iqbolsharifzoda724@gmail.com>
2025-08-24 22:52:23 +0300

    TwoPointers

--- TwoPointers/sort012.go
+++ TwoPointers/sort012.go
@@ -2,24 +2,26 @@ package main
 
 import "fmt"
 
-func sort012(arr []int) int {
+func sort012(arr []int) []int {
 	indexL := 0
-	//indexMid := indexR / 2
-	for i := 0; i < len(arr); i++ {
-		if arr[i] == 0 {
-			arr[indexL] = arr[i]
+	indexMid := 0
+	indexR := len(arr) - 1
+	for indexMid <= indexR {
+		if arr[indexMid] == 0 {
+			arr[indexL], arr[indexMid] = arr[indexMid], arr[indexL]
 			indexL++
-		} else if arr[i] == 1 {
-			arr[i], arr[i+1] = arr[i+1], arr[i]
+			indexMid++
+		} else if arr[indexMid] == 1 {
+			indexMid++
+		} else {
+			arr[indexMid], arr[indexR] = arr[indexR], arr[indexMid]
+			indexR--
 		}
 	}
-	return indexL
+	return arr
 }
 
 func main() {
-	arr := []int{0, 1, 2, 0, 1, 2}
-	sort012(arr)
-	for i := 0; i < len(arr); i++ {
-		fmt.Println(arr[i])
-	}
+	arr := []int{0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1}
+	fmt.Println(sort012(arr))
 }
